5e2235d20f1a9a0d9168a764f969e780bafa409d
Samuel A. Nimoh <33413170+samnimoh@users.noreply.github.com>
2023-07-24 04:44:40 +0000

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -11,9 +11,9 @@ def teardown_function(function):
     del function.x
 
 
-### Run to see failed test
-#def test_hello_add():
-#    assert add(test_hello_add.x) == 12
+## Run to see failed test
+def test_hello_add():
+   assert add(test_hello_add.x) == 12
 
 def test_hello_subtract():
     assert subtract(test_hello_subtract.x) == 9
